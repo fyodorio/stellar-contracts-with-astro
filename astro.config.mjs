c3b3877211564d7f75c853c065f969ebfa19e442
fyodorio <alinetofyodor@gmail.com>
2024-07-22 21:00:01 +0500

    feat: make incrementor and hello work with ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      // https://www.storyblok.com/faq/setting-up-https-on-localhost-in-astro
+      https: true,
+    },
+  },
+});
